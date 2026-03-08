c73c235d6855427752c4a7da54ed377899788943
gawakawa <mashurakiryu@icloud.com>
2026-03-08 20:42:29 +0900

    ✨ Implement minimal web server serving HTML on localhost:3000

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpListener;
+
+fn main() -> std::io::Result<()> {
+    let markdown = "Hello";
+    let html = markdown;
+
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    for stream in listener.incoming() {
+        let mut stream = stream?;
+
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{html}");
+        stream.write_all(response.as_bytes())?;
+    }
+    Ok(())
 }
